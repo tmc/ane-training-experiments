beaf02009de0c7e062521b4b495f672b1fc29038
Travis Cline <travis.cline@gmail.com>
2026-03-14 12:37:20 -0700

    storiesane: split platform-specific concurrency into build-tagged files

--- ane/storiesane/forward_common.go
+++ ane/storiesane/forward_common.go
@@ -1,10 +1,6 @@
 package storiesane
 
-import (
-	"math"
-	"runtime"
-	"sync"
-)
+import "math"
 
 func causalAttentionCF(out, qf, kf, vf []float32, heads, headDim, seq int) {
 	scores := make([]float32, seq)
@@ -86,38 +82,6 @@ func rmsNormRRMS(rrms, x []float32, dim, seq int) {
 			rrms[t] = float32(1.0 / math.Sqrt(sum/float64(dim)+1e-5))
 		}
 	})
-}
-
-func parallelForCF(n int, fn func(start, end int)) {
-	if n <= 0 {
-		return
-	}
-	workers := runtime.GOMAXPROCS(0)
-	if workers < 2 || n < workers*4 {
-		fn(0, n)
-		return
-	}
-	if workers > n {
-		workers = n
-	}
-	chunk := (n + workers - 1) / workers
-	var wg sync.WaitGroup
-	for worker := 0; worker < workers; worker++ {
-		start := worker * chunk
-		if start >= n {
-			break
-		}
-		end := start + chunk
-		if end > n {
-			end = n
-		}
-		wg.Add(1)
-		go func(start, end int) {
-			defer wg.Done()
-			fn(start, end)
-		}(start, end)
-	}
-	wg.Wait()
 }
 
 func silu32(x float32) float32 {
--- ane/storiesane/grad_tasks.go
+++ ane/storiesane/grad_tasks.go
@@ -1,9 +1,6 @@
 package storiesane
 
-import (
-	"runtime"
-	"sync"
-)
+import "runtime"
 
 const defaultGradTaskConcurrency = 3
 
@@ -20,33 +17,6 @@ func SetGradTaskConcurrency(n int) {
 	gradTaskLimit = n
 }
 
-type gradTasks struct {
-	jobs   chan func()
-	tasks  sync.WaitGroup
-	worker sync.WaitGroup
-}
-
-func newGradTasks() *gradTasks {
-	n := gradTaskConcurrency()
-	if n <= 0 {
-		return nil
-	}
-	g := &gradTasks{
-		jobs: make(chan func(), n*3),
-	}
-	for i := 0; i < n; i++ {
-		g.worker.Add(1)
-		go func() {
-			defer g.worker.Done()
-			for fn := range g.jobs {
-				fn()
-				g.tasks.Done()
-			}
-		}()
-	}
-	return g
-}
-
 func gradTaskConcurrency() int {
 	n := runtime.GOMAXPROCS(0)
 	if n < 1 {
@@ -56,28 +26,4 @@ func gradTaskConcurrency() int {
 		n = gradTaskLimit
 	}
 	return n
-}
-
-func (g *gradTasks) Go(fn func()) {
-	if g == nil {
-		fn()
-		return
-	}
-	g.tasks.Add(1)
-	g.jobs <- fn
-}
-
-func (g *gradTasks) Wait() {
-	if g == nil {
-		return
-	}
-	g.tasks.Wait()
-}
-
-func (g *gradTasks) Close() {
-	if g == nil {
-		return
-	}
-	close(g.jobs)
-	g.worker.Wait()
 }
