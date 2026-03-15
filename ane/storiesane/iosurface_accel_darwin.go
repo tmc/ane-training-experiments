d1cc22d8ab2fe5efe8e2f9898fad72c541366067
Travis Cline <travis.cline@gmail.com>
2026-03-15 09:28:00 -0700

    Revert "storiesane: fuse FFN backward copy+write into single IOSurface lock"
    This reverts commit 1917e5cdee0b1a5dccf8b9988f02cc5d94870d77.

--- ane/storiesane/iosurface_accel_darwin.go
+++ ane/storiesane/iosurface_accel_darwin.go
@@ -4,12 +4,8 @@ package storiesane
 
 /*
 #cgo darwin LDFLAGS: -framework IOSurface
-#cgo CFLAGS: -O3
 #include <IOSurface/IOSurface.h>
 #include <string.h>
-#ifdef __aarch64__
-#include <arm_neon.h>
-#endif
 
 static void *iosurface_lock_and_get_base(IOSurfaceRef surf, uint32_t options) {
 	IOSurfaceLock(surf, options, NULL);
@@ -77,97 +73,6 @@ static int iosurface_copy_range2(
 			memcpy((char*)dstBase + dOff, (char*)srcBase + sOff, rowBytes);
 	}
 	iosurface_unlock(srcSurf, 1);
-	iosurface_unlock(dstSurf, 0);
-	return 0;
-}
-// iosurface_copy_and_write_fp16 copies FP16 data from a source output surface
-// AND writes FP32→FP16 converted data to the same destination input surface in
-// a single lock/unlock cycle. This fuses two IOSurface operations that target
-// the same destination, saving one lock+unlock pair (~33µs per call).
-//
-// The copy reads from srcSurf output channels [srcChannel, srcChannel+channels)
-// at offset srcOffset, writing to dstSurf input at (channel 0, offset 0).
-// The two FP32 writes go to the same destination at specified offsets.
-static int iosurface_copy_and_write_fp16(
-	IOSurfaceRef dstSurf, int dstPlaneStride, int dstAllocSize,
-	IOSurfaceRef srcSurf, int srcPlaneStride, int srcAllocSize,
-	int srcChannel, int srcOffset,
-	int channels, int copyRowBytes, int elemSize,
-	const float *data1, int d1Offset, int d1Len,
-	const float *data2, int d2Offset, int d2Len,
-	int writeWidth
-) {
-	void *dstBase = iosurface_lock_and_get_base(dstSurf, 0);
-	void *srcBase = iosurface_lock_and_get_base(srcSurf, 1);
-	if (!dstBase || !srcBase) {
-		if (srcBase) iosurface_unlock(srcSurf, 1);
-		if (dstBase) iosurface_unlock(dstSurf, 0);
-		return -1;
-	}
-	// Copy FP16 from src output to dst input at channel 0, offset 0.
-	int dstElemStride = dstPlaneStride / elemSize;
-	for (int c = 0; c < channels; c++) {
-		int dstOff = c * dstPlaneStride;
-		int srcOff = (srcChannel + c) * srcPlaneStride + srcOffset * elemSize;
-		if (dstOff + copyRowBytes <= dstAllocSize && srcOff + copyRowBytes <= srcAllocSize)
-			memcpy((char*)dstBase + dstOff, (char*)srcBase + srcOff, copyRowBytes);
-	}
-	iosurface_unlock(srcSurf, 1);
-	// Write FP32→FP16 data1 and data2 into the still-locked dst surface.
-	_Float16 *dst16 = (_Float16 *)dstBase;
-#ifdef __aarch64__
-	for (int c = 0; c < channels && c * writeWidth < d1Len; c++) {
-		int srcOff = c * writeWidth;
-		int n = writeWidth;
-		if (srcOff + n > d1Len) n = d1Len - srcOff;
-		int dOff = c * dstElemStride + d1Offset;
-		if (dOff + n > dstAllocSize / 2) break;
-		const float *s = data1 + srcOff;
-		_Float16 *d = dst16 + dOff;
-		int i = 0;
-		for (; i + 7 < n; i += 8) {
-			float16x8_t h = vcombine_f16(
-				vcvt_f16_f32(vld1q_f32(s + i)),
-				vcvt_f16_f32(vld1q_f32(s + i + 4)));
-			vst1q_f16((__fp16 *)(d + i), h);
-		}
-		for (; i < n; i++) d[i] = (_Float16)s[i];
-	}
-	for (int c = 0; c < channels && c * writeWidth < d2Len; c++) {
-		int srcOff = c * writeWidth;
-		int n = writeWidth;
-		if (srcOff + n > d2Len) n = d2Len - srcOff;
-		int dOff = c * dstElemStride + d2Offset;
-		if (dOff + n > dstAllocSize / 2) break;
-		const float *s = data2 + srcOff;
-		_Float16 *d = dst16 + dOff;
-		int i = 0;
-		for (; i + 7 < n; i += 8) {
-			float16x8_t h = vcombine_f16(
-				vcvt_f16_f32(vld1q_f32(s + i)),
-				vcvt_f16_f32(vld1q_f32(s + i + 4)));
-			vst1q_f16((__fp16 *)(d + i), h);
-		}
-		for (; i < n; i++) d[i] = (_Float16)s[i];
-	}
-#else
-	for (int c = 0; c < channels && c * writeWidth < d1Len; c++) {
-		int srcOff = c * writeWidth;
-		int n = writeWidth;
-		if (srcOff + n > d1Len) n = d1Len - srcOff;
-		int dOff = c * dstElemStride + d1Offset;
-		if (dOff + n > dstAllocSize / 2) break;
-		for (int i = 0; i < n; i++) dst16[dOff + i] = (_Float16)data1[srcOff + i];
-	}
-	for (int c = 0; c < channels && c * writeWidth < d2Len; c++) {
-		int srcOff = c * writeWidth;
-		int n = writeWidth;
-		if (srcOff + n > d2Len) n = d2Len - srcOff;
-		int dOff = c * dstElemStride + d2Offset;
-		if (dOff + n > dstAllocSize / 2) break;
-		for (int i = 0; i < n; i++) dst16[dOff + i] = (_Float16)data2[srcOff + i];
-	}
-#endif
 	iosurface_unlock(dstSurf, 0);
 	return 0;
 }
@@ -455,51 +360,4 @@ func readOutputFP16CGo(k *model.Kernel, output int, data []float32) error {
 		readChannelFirstActsOffsetFP16(data, surfData, layout, 0, 0, layout.Width)
 		return nil
 	})
-}
-
-// copyAndWriteFP16CGo copies FP16 channels from src output to dst input AND
-// writes two FP32→FP16 data regions to the same dst input in a single
-// lock/unlock cycle. Saves one lock+unlock pair vs separate copy + write calls.
-func copyAndWriteFP16CGo(
-	dst *model.Kernel, dstInput int,
-	src *model.Kernel, srcOutput, srcChannel, channels int,
-	data1 []float32, d1WidthOffset int,
-	data2 []float32, d2WidthOffset int,
-	writeWidth int,
-) error {
-	if dst == nil || src == nil {
-		return fmt.Errorf("copy and write fp16 cgo: nil kernel")
-	}
-	if channels <= 0 {
-		return nil
-	}
-	dstLayout := dst.InputLayout(dstInput)
-	srcLayout := src.OutputLayout(srcOutput)
-	if dstLayout.Height != 1 || srcLayout.Height != 1 {
-		return fmt.Errorf("copy and write fp16 cgo: height > 1 not supported")
-	}
-	if dstLayout.ElemSize != srcLayout.ElemSize || dstLayout.ElemSize != 2 {
-		return fmt.Errorf("copy and write fp16 cgo: elem size dst=%d src=%d, want 2", dstLayout.ElemSize, srcLayout.ElemSize)
-	}
-	dstRef := dst.InputSurface(dstInput)
-	srcRef := src.OutputSurface(srcOutput)
-	if dstRef == 0 || srcRef == 0 {
-		return fmt.Errorf("copy and write fp16 cgo: nil surface ref")
-	}
-	copyRowBytes := writeWidth * srcLayout.ElemSize // FP16 copy width = writeWidth * 2
-	rc := C.iosurface_copy_and_write_fp16(
-		C.IOSurfaceRef(unsafe.Pointer(dstRef)),
-		C.int(dstLayout.PlaneStride), C.int(dstLayout.AllocSize()),
-		C.IOSurfaceRef(unsafe.Pointer(srcRef)),
-		C.int(srcLayout.PlaneStride), C.int(srcLayout.AllocSize()),
-		C.int(srcChannel), C.int(0),
-		C.int(channels), C.int(copyRowBytes), C.int(srcLayout.ElemSize),
-		(*C.float)(unsafe.Pointer(unsafe.SliceData(data1))), C.int(d1WidthOffset), C.int(len(data1)),
-		(*C.float)(unsafe.Pointer(unsafe.SliceData(data2))), C.int(d2WidthOffset), C.int(len(data2)),
-		C.int(writeWidth),
-	)
-	if rc != 0 {
-		return errNilIOSurfaceBase
-	}
-	return nil
 }
