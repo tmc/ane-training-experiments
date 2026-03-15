d7a773456ff48458ed93c76a00d986e0d7dec925
Travis Cline <travis.cline@gmail.com>
2026-03-15 07:39:23 -0700

    storiesane: fuse dsilu copy + h1/h3 write into single IOSurface lock in backward FFN

--- ane/storiesane/layer_dynamic_darwin.go
+++ ane/storiesane/layer_dynamic_darwin.go
@@ -423,11 +423,8 @@ func (lb *layerBackward) runDynamicFFN(dxNorm, dh1, dh3, dFFN, h1, h3 []float32)
 	if err := evalKernelTracked(lb.metrics, lb.ffnW2); err != nil {
 		return fmt.Errorf("run layer backward dynamic ffn: eval w2: %w", err)
 	}
-	if err := copyOutputRangeToInputCGo(lb.ffn, 0, 0, 0, lb.ffnW2, 0, 0, 0, lb.hidden, lb.seq); err != nil {
-		return fmt.Errorf("run layer backward dynamic ffn: copy dsilu to tail: %w", err)
-	}
-	if err := writeStoriesFFNTailAuxActs(lb.ffn, lb.seq, lb.hidden, h1, h3); err != nil {
-		return fmt.Errorf("run layer backward dynamic ffn: write tail aux input: %w", err)
+	if err := copyAndWriteFFNTailCGo(lb.ffn, 0, lb.ffnW2, 0, h1, h3, lb.hidden, lb.seq); err != nil {
+		return fmt.Errorf("run layer backward dynamic ffn: copy dsilu + write h1/h3: %w", err)
 	}
 	if err := evalKernelTracked(lb.metrics, lb.ffn); err != nil {
 		return fmt.Errorf("run layer backward dynamic ffn: eval tail: %w", err)
